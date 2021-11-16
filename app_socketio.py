0b14c68f645cb2bdc17ca8b8e0526f8fde8cbba5
Luu Truong Sinh <sinhlt58@gmail.com>
2021-11-17 01:48:03 +0700

    update user session data, room

--- app_socketio.py
+++ app_socketio.py
@@ -1,18 +1,18 @@
 import os
 import json
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
 from werkzeug.exceptions import HTTPException
-from flask_socketio import SocketIO, emit
-import random
-import datetime
-import base64
+from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from models.user_session_data import UserSesssionDataManager
 
 from stt.viettel_client import ViettelSTTClient
 
 app = Flask(__name__, static_folder="front-end-react/build")
 cors = CORS(app, resources={"*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+ss_data_manager = UserSesssionDataManager()
 
 viettel_stt_client = ViettelSTTClient()
 @socketio.on("stt")
@@ -21,6 +21,43 @@ def handle_stt(audio_chunk_bytes):
     print("requesting viettel stt ...")
     stt_text = viettel_stt_client.decode(audio_chunk_bytes)
     emit("stt", stt_text)
+
+
+@socketio.on("join")
+def on_join(data):
+    user_id = request.sid
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+    ss_data = ss_data_manager.get(user_id)
+    if ss_data is not None:
+        ss_data.join_room(room, username)
+
+    send(username + " has entered the room.", to=room)
+
+@socketio.on("leave")
+def on_leave(data):
+    user_id = request.sid
+    username = data["username"]
+    room = data["room"]
+    leave_room(room)
+    ss_data = ss_data_manager.get(user_id)
+    if ss_data is not None:
+        ss_data.leave_room()
+
+    send(username + " has left the room.", to=room)
+
+
+@socketio.on("connect")
+def handle_connect():
+    user_id = request.sid # Just use the session as id for convenience
+    ss_data_manager.create(user_id)
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    user_id = request.sid
+    ss_data_manager.clean(user_id)
 
 
 @app.errorhandler(404)
@@ -47,13 +84,13 @@ def handle_exception(e):
     return response
 
 
-@app.route("/", defaults={'path': ''})
+@app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def fallback(path):
     if path != "" and os.path.exists(f"{app.static_folder}/{path}"):
         return send_from_directory(app.static_folder, path)
     else:
-        return send_from_directory(app.static_folder, 'index.html')
+        return send_from_directory(app.static_folder, "index.html")
 
 
 @app.route("/health")
@@ -61,5 +98,5 @@ def health():
     return jsonify({"status": "ok"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     socketio.run(app, port=5000)
---
+++ models/user_session_data.py
@@ -0,0 +1,45 @@
+
+
+class UserSessionData(object):
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+    
+        self.example_vad_note_buffer = []
+        self.example_vad_chat_buffer = []
+        # connect to trident, ...
+
+        self.joined_room = None
+        self.username = "default_" + user_id
+
+    def on_clean(self):
+        # clean data
+        # disconnect to trident, ...
+        pass
+
+    def join_room(self, room, username):
+        self.joined_room = room
+        self.username = username
+
+    def leave_room(self):
+        self.joined_room = None
+        self.username = "default_" + self.user_id
+
+
+class UserSesssionDataManager(object):
+
+    def __init__(self):
+        self.user_to_data = {}
+
+    def get(self, user_id):
+        if user_id in self.user_to_data:
+            return self.user_to_data[user_id]
+        return None
+
+    def create(self, user_id):
+        self.user_to_data[user_id] = UserSessionData(user_id)
+
+    def clean(self, user_id):
+        if user_id in self.user_to_data:
+            self.user_to_data[user_id].on_clean()
+            del self.user_to_data[user_id]
